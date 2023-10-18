994f29d96efefc97e8c224c4b190af2efa11928d
VINAY KUMAR SINGH <mrsinghvinay563@gmail.com>
2023-10-18 20:52:09 +0530

    Java-mail-sender-ass-init

---
+++ CAS04-JavaMailSender/src/main/java/com/app/modal/Users.java
@@ -0,0 +1,30 @@
+package com.app.modal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Entity
+@Data
+public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String username;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    // Other fields, getters, setters, and constructors as needed
+}
+
---
+++ CAS04-JavaMailSender/src/main/java/com/app/service/UserService.java
@@ -0,0 +1,36 @@
+package com.app.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.app.exception.UsersRegistrationException;
+import com.app.modal.Users;
+import com.app.repo.UserRepository;
+
+@Service
+public class UserService {
+	    @Autowired
+	    private UserRepository userRepository;
+
+	    @Autowired
+	    private PasswordEncoder passwordEncoder; // Use BCryptPasswordEncoder
+
+	    public Users registerUsers(Users user) {
+	        if (userRepository.findByUsername(user.getUsername()).isPresent()) {
+	            throw new UsersRegistrationException("Usersname is already taken");
+	        }
+
+	        if (userRepository.findByEmail(user.getEmail()).isPresent()) {
+	            throw new UsersRegistrationException("Email is already registered");
+	        }
+
+	        // Encrypt password before saving to the database
+	        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+	        return userRepository.save(user);
+	    }
+	
+
+	
+}
