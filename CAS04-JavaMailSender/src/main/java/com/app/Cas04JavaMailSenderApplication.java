a4f32ec4235f43d319b8b648a0c43dbfdeb582a9
VINAY KUMAR SINGH <mrsinghvinay563@gmail.com>
2023-10-18 20:24:55 +0530

    Java-mail-sender

---
+++ CAS04-JavaMailSender/src/main/java/com/app/Cas04JavaMailSenderApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cas04JavaMailSenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cas04JavaMailSenderApplication.class, args);
+	}
+
+}
