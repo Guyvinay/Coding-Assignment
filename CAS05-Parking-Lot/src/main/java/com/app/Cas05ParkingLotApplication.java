fb38e6fa6305681d411a3f5cdffd36904217089d
VINAY KUMAR SINGH <mrsinghvinay563@gmail.com>
2023-10-27 02:50:07 +0530

    Coding-assignment-5-Parking-Lot-init

---
+++ CAS05-Parking-Lot/src/main/java/com/app/Cas05ParkingLotApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cas05ParkingLotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cas05ParkingLotApplication.class, args);
+	}
+
+}
