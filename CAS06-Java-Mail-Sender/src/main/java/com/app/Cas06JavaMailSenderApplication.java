b12c92111cf0ec98924a882b2e9cde386f73b0c8
VINAY KUMAR SINGH <mrsinghvinay563@gmail.com>
2023-10-27 03:40:05 +0530

    Coding-assignment-Enhanced-Java-Mail-Sender-init

---
+++ CAS06-Java-Mail-Sender/src/main/java/com/app/Cas06JavaMailSenderApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cas06JavaMailSenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cas06JavaMailSenderApplication.class, args);
+	}
+
+}
