a14100b1e60c9e34ff5405de878e026a4319b362
VINAY KUMAR SINGH <mrsinghvinay563@gmail.com>
2023-10-27 16:46:08 +0530

    coding-assignment-07-User-Registration-Exception-handling-customuserdetails-impl

--- CAS07-User-Registration/src/main/java/com/app/service/UserServiceImpl.java
+++ CAS07-User-Registration/src/main/java/com/app/service/UserServiceImpl.java
@@ -1,5 +1,37 @@
 package com.app.service;
 
-public interface UserServiceImpl {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.app.modal.Users;
+import com.app.repository.UsersRepository;
+
+@Service
+public class UserServiceImpl implements UsersService {
+
+	@Autowired
+	private UsersRepository usersRepository;
+	
+	@Override
+	public Users registerUser(Users user) {
+		
+		
+		
+		return null;
+	}
+
+	@Override
+	public List<Users> getAllUsers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Users getUserDetails(String email) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
