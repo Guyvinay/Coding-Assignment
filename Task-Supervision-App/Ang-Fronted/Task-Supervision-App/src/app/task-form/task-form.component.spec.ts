b72beb4a962abae329a21752252d57765f74695a
VINAY KUMAR SINGH <mrsinghvinay563@gmail.com>
2023-11-08 16:29:30 +0530

    Unit-testing-for-frontend-added

--- Task-Supervision-App/Ang-Fronted/Task-Supervision-App/src/app/task-form/task-form.component.spec.ts
+++ Task-Supervision-App/Ang-Fronted/Task-Supervision-App/src/app/task-form/task-form.component.spec.ts
@@ -1,18 +1,38 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TaskFormComponent } from './task-form.component';
+import { TaskService } from '../task.service';
 
 describe('TaskFormComponent', () => {
   let component: TaskFormComponent;
   let fixture: ComponentFixture<TaskFormComponent>;
+  let taskService: jasmine.SpyObj<TaskService>;
+
+  let service: TaskService;
 
   beforeEach(() => {
+    taskService = jasmine.createSpyObj('TaskService', ['getAllTasks', 'createTask']);
+
     TestBed.configureTestingModule({
-      declarations: [TaskFormComponent]
+      declarations: [TaskFormComponent],
+      providers: [TaskService],
     });
     fixture = TestBed.createComponent(TaskFormComponent);
+    service = TestBed.inject(TaskService);
+
     component = fixture.componentInstance;
     fixture.detectChanges();
+  });
+
+  it('should get all tasks', () => {
+    service.getAllTasks().subscribe((response) => {
+      expect(response).toBeTruthy();
+    });
+  });
+
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
   });
 
   it('should create', () => {
