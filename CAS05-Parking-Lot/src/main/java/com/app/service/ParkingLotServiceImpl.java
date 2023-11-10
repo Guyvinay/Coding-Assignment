247b949409c98c6b888b33758336b50e0e9df4a2
VINAY KUMAR SINGH <mrsinghvinay563@gmail.com>
2023-11-10 22:16:12 +0530

    CAS09-Bloggin-web-service-init

--- CAS05-Parking-Lot/src/main/java/com/app/service/ParkingLotServiceImpl.java
+++ CAS05-Parking-Lot/src/main/java/com/app/service/ParkingLotServiceImpl.java
@@ -1,5 +1,39 @@
 package com.app.service;
 
-public class ParkingLotServiceImpl {
+import java.util.List;
+
+import com.app.model.ParkingLot;
+
+public class ParkingLotServiceImpl implements ParkingLotService {
+
+	@Override
+	public List<ParkingLot> getAllParkingLots() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ParkingLot getParkingLotById(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void createParkingLot(ParkingLot parkingLot) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateParkingLot(ParkingLot parkingLot) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteParkingLot(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
