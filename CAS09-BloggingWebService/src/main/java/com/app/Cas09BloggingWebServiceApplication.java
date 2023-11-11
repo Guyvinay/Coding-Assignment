4e260533ddd7bc8e48d01a0d496dc0bdf903b438
VINAY KUMAR SINGH <mrsinghvinay563@gmail.com>
2023-11-11 15:25:39 +0530

    USer-Persistence-acc

---
+++ CAS09-BloggingWebService/src/main/java/com/app/Cas09BloggingWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cas09BloggingWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cas09BloggingWebServiceApplication.class, args);
+	}
+
+}
