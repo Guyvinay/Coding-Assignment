4155792583564db5099689904c2d4550056aca96
unknown <mrsinghvinay563@gmail.com>
2024-02-09 19:37:47 +0530

    refactoring

--- Prospecta/Eval-2/ProspectaEval/src/main/java/com/app/appRepository/AppRepository.java
+++
@@ -1,9 +0,0 @@
-package com.app.appRepository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.app.modal.Entities;
-
-public interface AppRepository extends JpaRepository<Entities, String> {
-
-}
